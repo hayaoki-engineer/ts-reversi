59a997e74f8f80c9d4f63dfe627a144c844e4fd8
Taisei <taisei.960420@gmail.com>
2024-11-10 11:51:58 +0900

    async await

--- training/asynchronous.ts
+++ training/asynchronous.ts
@@ -4,9 +4,13 @@ import util from 'util';
 const promisifyReadFile = util.promisify(fs.readFile);
 
 async function main() {
+  console.log("1. 開始");
+
+  console.log("2. ファイル読み込み前");
   const data = await promisifyReadFile('../package.json');
+  console.log("3. ファイル読み込み後：", data);
   const fileContent = data.toString();
-  console.log(fileContent)
+  console.log('4. 完了:',fileContent)
 }
 
 main()
